17dabb049a59939f86fc5cdf8611b0b535e7e613
felec <felecrear@gmail.com>
2021-11-13 10:42:55 -0800

    fix cdn replace

--- utils/cdn.ts
+++ utils/cdn.ts
@@ -1,6 +1,9 @@
 export const cdn = (url: string) => {
+  if (!url) return '';
+
   const regex = /\bliked-tweets-108136115.s3.amazonaws.com\b/;
   let cdn = url;
+
   cdn = cdn.replace(regex, 'liked-tweets.b-cdn.net');
   return cdn;
 };
